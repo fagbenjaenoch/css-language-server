d9568fd80a2ab447099055bd6e2c5690690c8fa3
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-01-01 16:00:15 +0100

    setup basic logging

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello")
+	log := getLogger("/home/enoch/dev/css-language-server/log.txt")
+	log.Println("server started")
+}
+
+func getLogger(filename string) *log.Logger {
+	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("could not open log file: %v", err)
+	}
+
+	return log.New(logFile, "[css-language-server]", log.Ldate|log.Ltime|log.Lshortfile)
 }
