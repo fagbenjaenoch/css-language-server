c880886ca6983838193b5693e09ac631004bdc5e
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-01-03 21:01:57 +0100

    implemented server response to client initialize request

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"io"
 	"log"
 	"os"
 
@@ -11,39 +12,44 @@ import (
 )
 
 func main() {
-	log := getLogger("/home/enoch/dev/css-language-server/log.txt")
-	log.Println("server started")
+	logger := getLogger("/home/enoch/dev/css-language-server/log.txt")
+	logger.Println("server started")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
+	writer := os.Stdout
 
 	for scanner.Scan() {
 		rpcRequest := scanner.Bytes()
-		log.Printf("received a message from the client of %d bytes", len(rpcRequest))
+		logger.Printf("received a message from the client of %d bytes", len(rpcRequest))
 
 		method, body, err := rpc.DecodeMessage(rpcRequest)
 		if err != nil {
-			log.Printf("could not parse request body: %s", err)
+			logger.Printf("could not parse request body: %s", err)
 			continue
 		}
-		log.Printf("received a request of %s method", method)
 
-		switch method {
-		case rpc.MethodInitialize:
-			{
-				log.Println("initializing server")
-				var request lsp.InitializeParams
-				if err := json.Unmarshal(body, &request); err != nil {
-					log.Printf("could not unmarshal request body: %s", err)
-					continue
-				}
+		handleRequest(logger, writer, method, body)
+	}
+}
 
-				log.Printf("connected to %s %s", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
+func handleRequest(logger *log.Logger, writer io.Writer, method string, body []byte) {
+	logger.Printf("recieved method %s from client", method)
 
-				initializeResponse := lsp.NewInitializeResponse(request.ID)
-				response := rpc.EncodeMessage(initializeResponse)
-				os.Stdout.Write([]byte(response))
+	switch method {
+	case rpc.MethodInitialize:
+		{
+			log.Println("initializing server")
+			var request lsp.InitializeParams
+			if err := json.Unmarshal(body, &request); err != nil {
+				log.Printf("could not unmarshal request body: %s", err)
 			}
+
+			log.Printf("connected to %s %s", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
+
+			initializeResponse := lsp.NewInitializeResponse(request.ID)
+			response := rpc.EncodeMessage(initializeResponse)
+			writer.Write([]byte(response))
 		}
 	}
 }
